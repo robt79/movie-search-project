d89e6118d0e979b963e0219668a7f813a9eaec03
Robert Torres <robt792003@gmail.com>
2017-05-19 10:08:39 -0500

    Updates
    cleaned my code up a bit

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 $(function() {
   console.log('hello world');
-  let randomMovieArray = ['Star Wars', 'Logan', 'Harry Potter', 'Lord of the Rings', 'Goonies', 'Groundhog Day', 'Unforgiven', 'Dirty Harry', 'When Harry met Sally ', 'Scream', 'Airheads', 'Star Trek', 'The Matrix', 'The Hangover', 'E.T.', 'Three Amigos', 'Rocky', 'Halloween', 'The Terminator', 'A Nightmare on Elm street', 'Footloose', 'The Little Shop of Horrors', 'Deadpool', 'The Notebook', 'Braveheart', 'The Big Lebowski', 'Forrest Gump', 'Casablanca', 'Godzilla', 'Spaceballs', 'Honey I Shrunk the Kids', 'Snatch', 'The Martian', 'Arrival', 'Dick Tracy', 'Alien', 'King Kong', 'Tombstone', 'Home Alone', 'Die Hard', 'Citizen Kane', 'Ghostbusters', 'No Country for Old Men', 'Friday', 'X-Men', 'Moonlight', 'Captain America: Civil War', 'Iron Man', 'Spider-Man', 'Manchester by the Sea', 'Top Gun', 'Doctor Strange', 'Guardians of the Galaxy', 'Mallrats', 'The Revenant', 'Ex Machina', 'Enter the Dragon', 'Spotlight', 'Beast of No Nation', 'The Hateful Eight', 'Scarface', 'Gone With the Wind', 'Anchorman:The Legend of Ron Burgundy', 'Fight Club', 'Goodwill Hunting', 'Gone With the Wind', 'Pulp Fiction', 'Goddfellas']
+  let randomMovieArray = ['Star Wars', 'Logan', "Harry Potter and the Sorcerer's Stone", 'Lord of the Rings', 'Goonies', 'Groundhog Day', 'Unforgiven', 'Dirty Harry', 'When Harry met Sally ', 'Scream', 'Airheads', 'Star Trek', 'The Matrix', 'The Hangover', 'E.T.', 'Three Amigos', 'Rocky', 'Halloween', 'The Terminator', 'A Nightmare on Elm street', 'Footloose', 'The Little Shop of Horrors', 'Deadpool', 'The Notebook', 'Braveheart', 'The Big Lebowski', 'Forrest Gump', 'Casablanca', 'Godzilla', 'Spaceballs', 'Honey I Shrunk the Kids', 'Snatch', 'The Martian', 'Arrival', 'Dick Tracy', 'Alien', 'King Kong', 'Tombstone', 'Home Alone', 'Die Hard', 'Citizen Kane', 'Ghostbusters', 'No Country for Old Men', 'Friday', 'X-Men', 'Moonlight', 'Captain America: Civil War', 'Iron Man', 'Spider-Man', 'Manchester by the Sea', 'Top Gun', 'Doctor Strange', 'Guardians of the Galaxy', 'Mallrats', 'The Revenant', 'Ex Machina', 'Enter the Dragon', 'Spotlight', 'Beasts of No Nation', 'The Hateful Eight', 'Scarface', 'Gone With the Wind', 'Anchorman:The Legend of Ron Burgundy', 'Fight Club', 'Good Will Hunting', 'Gone With the Wind', 'Pulp Fiction', 'Goodfellas', '2001: A Space Odyssey', 'Mad Max: Fury Road', 'The Grand Budapest Hotel', 'The Imitation Game', 'Transformers: Age of Extinction', 'Man of Steel', 'The Wolf of Wall Street', 'Django Unchained', 'The Dark Knight', 'Life of Pi', 'The Big Short', 'Bridesmaids', 'Drive', 'Gladiator', 'American Psycho', 'Gladiator', 'Cast Away', 'High Fidelity', 'Sin City', 'Walk the Line', 'The 40-Year-Old Virgin', 'Wedding Crashers', 'Lord of War', 'Little Miss Sunshine', "Pan's Labyrinth", 'Training Day', 'Zoolander', 'Talladega Nights: The Ballad Of Ricky Bobby']
 
 
   $('#button').on('click', function(event) {
 
     event.preventDefault();
+
+
     let randomNumber = Math.floor((Math.random() * randomMovieArray.length - 1) + 1);
     console.log(randomNumber);
     let randomMovie = randomMovieArray[randomNumber];
     console.log(randomMovie);
+
     let $xhr = $.getJSON('http://www.omdbapi.com/?t=' + randomMovie);
 
 
@@ -24,8 +27,6 @@ $(function() {
 
       $('#card').attr('src', data.Poster);
 
-
-      // $('#poster').html(data.Poster);
       $('#title').html(data.Title);
       $('#plot').html(data.Plot);
       $('#actors').html(data.Actors);
@@ -39,19 +40,20 @@ $(function() {
       $('#easteregg').click(function() {
         $('#body').addClass('unicorn');
         $('#contain').addClass(' pony');
-        $('#nav').addClass(' baby');
-        $('#foot').addClass(' baby');
-        $('#cards').addClass(' baby');
-        $('#cardss').addClass(' baby');
-        $('#cardsss').addClass(' baby');
-        $('#cardssss').addClass(' baby');
-        $('#button').addClass(' baby');
+        $('#nav').addClass(' rainbow');
+        $('#foot').addClass(' rainbow');
+        $('#cards').addClass(' rainbow');
+        $('#cardss').addClass(' rainbow');
+        $('#cardsss').addClass(' rainbow');
+        $('#cardssss').addClass(' rainbow');
+        $('#button').addClass(' rainbow');
         $('#cardsssss').addClass(' unicorn');
         $('#images').addClass(' unicorn');
         $('#corn').addClass(' unicorn');
         $('#back').addClass('card1');
         $('#back2').addClass('card1');
         $('#maincard').addClass('card1');
+        $('#list').addClass('writing');
       });
 
     });
@@ -68,10 +70,6 @@ $(function() {
 
 
 });
-
-
-
-
 
 
 //
